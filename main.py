79b469a225e89fcb57c3bbdd01b0ef05c4e709b2
Denis Medov <medovdenis@gmail.com>
2021-11-05 15:47:40 +0300

    nir

--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 import dash
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
-import dash_html_components as html
 
 import datetime as date
 
-from data import get_OHLC, get_graps_avr_hl, get_graps_avr_oc, get_graps_diff_hl, get_graps_diff_oc
+from data import *
 from company import COMPANY
+
+now = date.datetime.now()
 
 app = dash.Dash()
 app.layout = html.Div([
-    html.H1(children='Акции'),
-    dcc.DatePickerRange(id='date_range_stock', start_date=date.datetime.now(), end_date=date.datetime.now()),
+    html.H1(children='Биржевые индексы'),
+    dcc.DatePickerRange(id='date_range_stock', start_date=now, end_date=now, display_format='DD-MM-YY'),
     dcc.Dropdown(id='dropdown_stock', options=COMPANY, value=None),
     html.Button(id='button_stock', children='Submit', ),
     html.Div(id='figure_stock'),
 
     html.H1(children='Прогнозирование по среднему High-Low'),
-    dcc.DatePickerRange(id='date_range_avr_hl', start_date=date.datetime.now(), end_date=date.datetime.now()),
+    dcc.DatePickerRange(id='date_range_avr_hl', start_date=now, end_date=now, display_format='DD-MM-YY'),
     dcc.Dropdown(id='dropdown_avr_hl', options=COMPANY, value=None),
     html.Button(id='button_avr_hl', children='Submit'),
     html.Div(id='figure_avr_hl'),
 
     html.H1(children='Прогнозирование по среднему Open-Close'),
-    dcc.DatePickerRange(id='date_range_avr_oc', start_date=date.datetime.now(), end_date=date.datetime.now()),
+    dcc.DatePickerRange(id='date_range_avr_oc', start_date=now, end_date=now, display_format='DD-MM-YY'),
     dcc.Dropdown(id='dropdown_avr_oc', options=COMPANY, value=None),
     html.Button(id='button_avr_oc', children='Submit'),
     html.Div(id='figure_avr_oc', children=[]),
 
     html.H1(children='Прогнозирование по разнице High-Low'),
-    dcc.DatePickerRange(id='date_range_diff_hl', start_date=date.datetime.now(), end_date=date.datetime.now()),
+    dcc.DatePickerRange(id='date_range_diff_hl', start_date=now, end_date=now, display_format='DD-MM-YY'),
     dcc.Dropdown(id='dropdown_diff_hl', options=COMPANY, value=None),
     html.Button(id='button_diff_hl', children='Submit'),
     html.Div(id='figure_diff_hl', children=[]),
 
     html.H1(children='Прогнозирование по разнице Open-Close'),
-    dcc.DatePickerRange(id='date_range_diff_oc', start_date=date.datetime.now(), end_date=date.datetime.now()),
+    dcc.DatePickerRange(id='date_range_diff_oc', start_date=now, end_date=now, display_format='DD-MM-YY'),
     dcc.Dropdown(id='dropdown_diff_oc', options=COMPANY, value=None),
     html.Button(id='button_diff_oc', children='Submit'),
     html.Div(id='figure_diff_oc', children=[]),
@@ -48,8 +50,8 @@ app.layout = html.Div([
     State('date_range_stock', 'start_date'),
     State('date_range_stock', 'end_date'))
 def update_output(_, value : str, start_date : date.datetime, end_date: date.datetime):
-    if value == None:
-        return
+    if value == None: return
+    if start_date == end_date: return
     return dcc.Graph(figure=get_OHLC(value, start_date, end_date))
 
 @app.callback(
@@ -59,8 +61,8 @@ def update_output(_, value : str, start_date : date.datetime, end_date: date.dat
     State('date_range_avr_hl', 'start_date'),
     State('date_range_avr_hl', 'end_date'))
 def update_output(_, value : str, start_date : date.datetime, end_date: date.datetime):
-    if value == None:
-        return
+    if value == None: return
+    if start_date == end_date: return
     return dcc.Graph(figure=get_graps_avr_hl(value, start_date, end_date))
 
 @app.callback(
@@ -70,8 +72,8 @@ def update_output(_, value : str, start_date : date.datetime, end_date: date.dat
     State('date_range_avr_oc', 'start_date'),
     State('date_range_avr_oc', 'end_date'))
 def update_output(_, value : str, start_date : date.datetime, end_date: date.datetime):
-    if value == None:
-        return
+    if value == None: return
+    if start_date == end_date: return
     return dcc.Graph(figure=get_graps_avr_oc(value, start_date, end_date))
 
 @app.callback(
@@ -81,8 +83,8 @@ def update_output(_, value : str, start_date : date.datetime, end_date: date.dat
     State('date_range_diff_hl', 'start_date'),
     State('date_range_diff_hl', 'end_date'))
 def update_output(_, value : str, start_date : date.datetime, end_date: date.datetime):
-    if value == None:
-        return
+    if value == None: return
+    if start_date == end_date: return
     return dcc.Graph(figure=get_graps_diff_hl(value, start_date, end_date))
 
 @app.callback(
@@ -92,10 +94,10 @@ def update_output(_, value : str, start_date : date.datetime, end_date: date.dat
     State('date_range_diff_oc', 'start_date'),
     State('date_range_diff_oc', 'end_date'))
 def update_output(_, value : str, start_date : date.datetime, end_date: date.datetime):
-    if value == None:
-        return
+    if value == None: return
+    if start_date == end_date: return
     return dcc.Graph(figure=get_graps_diff_oc(value, start_date, end_date))
 
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=False)
+    app.run_server(debug=True)
 
